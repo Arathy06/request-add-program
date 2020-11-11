8fe711766b0484a4ca93412208d5d2d569fac622
Arathy06 <arathysree97@gmail.com>
2020-11-11 11:47:13 +0530

    request add program

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app=express();
+
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+
+app.post('/add',(res,req)=>{
+
+    var getnum1=parseInt(req.body.num1)
+    var getnum2=parseInt(req.body.num2)
+    var result=getnum1+getnum2
+    res.status(200).json({"result":result})
+})
+
+
+app.listen(process.env.PORT || 3000,(error)=>{
+
+    if(error)
+    {
+        console.log("Error occured")
+    }
+    else
+    {
+        console.log("Server started")
+    }
+})
